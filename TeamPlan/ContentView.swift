df9a749cb27200cbec8d510f78283cd79fffd81d
crossbell8368 <godfather346@gmail.com>
2023-02-06 15:06:17 +0900

    conf: Add Empty ProjectFile set project name & identifier set iOS version 15.0

---
+++ TeamPlan/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TeamPlan
+//
+//  Created by 주찬혁 on 2023/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Welcone to Team1OS")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TeamPlan/TeamPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamPlanApp.swift
+//  TeamPlan
+//
+//  Created by 주찬혁 on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct TeamPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
