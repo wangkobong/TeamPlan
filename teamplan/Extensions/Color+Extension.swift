cc439cd7fc4a446a5092a4b1bc4500af5e41c4a6
wangkobong <wangkobong@gmail.com>
2023-02-15 23:58:05 +0900

    [project] add color assets and Color Extension
    피그마 참고하여 컬러애셋 추가 및 익스텐션 파일 추가

---
+++ teamplan/Extensions/Color+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  Color+Extension.swift
+//  teamplan
+//
+//  Created by sungyeon kim on 2023/02/15.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let blackColor = Color("BlackColor")
+    let darkGreyColor = Color("DarkGreyColor")
+    let greyColor = Color("GreyColor")
+    let mainBlueColor = Color("MainBlueColor")
+    let mainPurpleColor = Color("MainPurpleColor")
+    let warningRedColor = Color("WarningRedColor")
+    let whiteColor = Color("WhiteColor")
+    let whiteGreyColor = Color("WhiteGreyColor")
+}
