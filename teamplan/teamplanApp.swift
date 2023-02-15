ec8c18989bb36958c14ed26a0b63b99adc7dc15b
crossbell8368 <godfather346@gmail.com>
2023-02-15 17:01:20 +0900

    [refact] Add empty xcode project file 새로운 프로젝트 공유방식 적용을 위한 빈 프로젝트 파일 업로드

---
+++ teamplan/teamplanApp.swift
@@ -0,0 +1,17 @@
+//
+//  teamplanApp.swift
+//  teamplan
+//
+//  Created by 주찬혁 on 2023/02/15.
+//
+
+import SwiftUI
+
+@main
+struct teamplanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
