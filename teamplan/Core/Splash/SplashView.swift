936d48d16f4e1d5580dc2c92e60d1e60e87c2556
wangkobong <wangkobong@gmail.com>
2023-02-18 18:30:46 +0900

    #6 [feat] add launch screen

---
+++ teamplan/Core/Splash/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  teamplan
+//
+//  Created by sungyeon kim on 2023/02/18.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
