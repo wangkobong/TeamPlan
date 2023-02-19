8009c8c2bfe035cf3c8986fbda171e7235e684d3
wangkobong <wangkobong@gmail.com>
2023-02-20 00:51:53 +0900

    [feat] add login view

--- teamplan/Core/Loing/LoginView.swift
+++ teamplan/Core/Loing/LoginView.swift
@@ -9,12 +9,64 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text("로그인뷰")
+        VStack {
+            HStack {
+                Image("loginView")
+                    .padding(.trailing, 51)
+                    .padding(.leading, 16)
+            }
+            Spacer()
+                .frame(height: 100)
+            
+            LoginButtonsView()
+        }
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
+    }
+}
+
+struct LoginButtonsView: View {
+    var body: some View {
+        VStack(spacing: 18) {
+            Button(action: {}) {
+                HStack {
+                    Image("appleLogo")
+                    Spacer()
+                    Text("Apple로 계속하기")
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .frame(height: 48)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.theme.blackColor)
+                .background(
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(SwiftUI.Color.theme.blackColor, lineWidth: 1)
+                )
+            }
+            .padding(.horizontal, 55)
+            
+            Button(action: {}) {
+                HStack {
+                    Image("appleLogo")
+                    Spacer()
+                    Text("Google로 계속하기")
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .frame(height: 48)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.theme.blackColor)
+                .background(
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(SwiftUI.Color.theme.blackColor, lineWidth: 1)
+                )
+            }
+            .padding(.horizontal, 55)
+        }
     }
 }
