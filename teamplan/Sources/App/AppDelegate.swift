99a4735de9223d7bc232825746a27a0ffb4d2cc6
crossbell8368 <godfather346@gmail.com>
2023-05-02 19:15:40 +0900

    [Feat] Add FirebaseCore & GoogleService-Info.plist * Firebase) 파이어베이스 연동에 필요한 GoogleService-Info.plist 파일추가         => Resource 파일에 추가시 Tuist에서 generate 시 자동으로 프로젝트에 등록 * Firebase) 파이어베이스 활성화코드 추가 => AppDelegate.swift & teamplanApp.swift

--- teamplan/Sources/App/AppDelegate.swift
+++ teamplan/Sources/App/AppDelegate.swift
@@ -7,10 +7,15 @@
 
 import Foundation
 import SwiftUI
+import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
         Thread.sleep(forTimeInterval: 1.5)
+        
+        FirebaseApp.configure()
+        
         return true
     }
 }
--- teamplan/Sources/App/teamplanApp.swift
+++ teamplan/Sources/App/teamplanApp.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 @main
 struct teamplanApp: App {
-    
+    // 온보딩 관련 State
     @State private var showIntroView: Bool = true
+    
+    // Firebase 관련
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
