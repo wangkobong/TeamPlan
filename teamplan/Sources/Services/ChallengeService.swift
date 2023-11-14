4a08a25f82de375627ee4e6229da5412d19c02bd
crossbell8368 <godfather346@gmail.com>
2023-11-14 22:27:04 +0900

    [Refact] Refactoring 'ChallengeService' * 'ChallengeService'   * 생성자     * 나의 도전과제 및 전체 도전과제 목록 및 사용자통계 추가     * 생성 준비함수 추가   * Get 함수통합     * 기존 : 도전과제 앞면조회 & 뒷면조회 이원화     * 변경안 : 앞뒷면 정보를 모두 담아서 전달   * MyChallenge / Challenge 이분화     * extension을 활용하여 함수분리   * MyChallenge     * Get       * 사용자 통계정보에 담긴 'stat_mychlg' 정보를 참조하여 탐색       * 최대 3가지의 나의 도전과제 정보탐색       * 관련 사용자통계를 추가하여 객체Array 형태로 return     * Set       * 선택된 도전과제 상태업데이트       * 사용자 통게정보에 추가된 도전과제 기록       * 나의 도전과제 목록 업데이트     * Update       * 해제(Disable) & 보상(Reward) 구분       * Disable         * 해제된 도전과제 상태업데이트         * 사용자 통계정보에서 도전과제 정보삭제         * 나의 도전과제 목록 업데이트       * Reward         * 보상받은 도전과제 상태업데이트         * unlock된 도전과제 상태업데이트 및 정보 return         * 사용자 통계정보에서 도전과제 정보삭제         * 도전과제 로그에 보상받은 도전과제 및 시각기록     * Etc       * 기능동작을 위한 support함수 구축   * Challenge     * Get       * ChallengeDTO 객체에 앞뒷면 정보를 모두담아 return       * 선행 도전과제 정보를 담아서 return       * 없는경우 nil 상태값으로   * 커스텀 Exception     * 변경된 함수내용에 맞춰 수정 * Etc   * 정상빌드를 위해 HomeView 내 ChallengeService 관련코드 주석처리

--- teamplan/Sources/Services/ChallengeService.swift
+++ teamplan/Sources/Services/ChallengeService.swift
@@ -13,188 +13,254 @@ final class ChallengeService{
     //===============================
     // MARK: - Global Parameter
     //===============================
-    // CoreData
-    let chlgCD = ChallengeServicesCoredata()
-    // StatisticsCenter
-    //let statCenter: StatisticsCenter
-    // Total Challenge Data
-    var challengeArray: [ChallengeObject] = []
-    // Identifier
-    var identifier: String
+    let challengeCD = ChallengeServicesCoredata()
+    let challengeLogCD = ChallengeLogServicesCoredata()
+    let statisticsCD = StatisticsServicesCoredata()
+    let statCenter = StatisticsCenter()
     
-    @Published var myChallenges: [ChallengeCardResDTO] = []
+    var userId: String
+    var statistics: StatisticsDTO?
+    var challengeArray: [ChallengeObject]?
+    
+    @Published var myChallenges: [MyChallengeDTO]?
     
     //===============================
     // MARK: - Constructor
     //===============================
-    init(identifier: String){
-        self.identifier = identifier
-        //self.statCenter = StatisticsCenter(identifier: identifier)
-        
-        self.getMyChallenge { result in
-            switch result {
-            case .success(let challenges):
-                self.myChallenges = challenges
-            case .failure(_):
-                self.myChallenges = []
-            }
-        }
+    init(_ id: String){
+        self.userId = id
     }
     
-    // Pre Load Challenge Data
-    /*
-    func initChallenge(result: @escaping(Result<Bool, Error>) -> Void) {
-        chlgCD.getChallenges { cdResult in
-            switch cdResult {
-            case .success(let reqChlg):
-                self.challengeArray = reqChlg
-                return result(.success(true))
-            case.failure(let error):
-                return result(.failure(error))
-            }
-        }
+    // Preload Statistics for 'ChallengeView' Constructor
+    func loadStatistics() throws {
+        self.statistics = StatisticsDTO(statObject: try statisticsCD.getStatistics(from: self.userId))
     }
-     */
-
-    //===============================
-    // MARK: Get - MyChallenges
-    //===============================
-    //TODO: Exception Handling
-    func getMyChallenge(result: @escaping(Result<[ChallengeCardResDTO], Error>) -> Void) {
-        
-        // extract challenge, that 'chlg_selected' is true
-        let myChallenges = challengeArray.filter { $0.chlg_selected }.map { ChallengeCardResDTO(chlgObject: $0) }
-        
-        // return myChallengeCard Array
-        return result(myChallenges.isEmpty ? .failure(ChallengeError.NoMyChallenge) : .success(myChallenges))
+    
+    // Preload Challenge Data
+    func loadChallenges() throws {
+        try self.challengeArray = challengeCD.getChallenges()
+    }
+    
+    // Preload MyChallenge Data
+    func loadMyChallenges() throws {
+        try self.getMychallengesProcess()
     }
     
     //===============================
-    // MARK: Get - MyChallenge Detail
+    // MARK: Set MyChallenges
     //===============================
-    /*TODO: Exception Handling
-    func getMyChallengeDetail(challengeId: Int,
-                              result: @escaping(Result<MyChallengeDetailResDTO, Error>) -> Void) {
+    func setMyChallenge(from challengeId: Int) async throws {
+        // Search & Update Challenge
+        let updatedChallenge = ChallengeStatusDTO(target: challengeId, select: true, selectTime: Date())
+        try await challengeCD.updateChallenge(from: updatedChallenge)
         
-        // extract challenge, that 'chlg_selected' is true
-        guard let challenge = challengeArray.first(where: { $0.chlg_id == challengeId }) else {
-            return result(.failure(ChallengeError.InvalidChallengeId))
-        }
-        // get userPorgress
-        statCenter.returnUserProgress(challengeType: challenge.chlg_type) { centerRes in
-            switch centerRes {
-            case .success(let progress):
-                
-                // struct challengeDetail
-                let detail = MyChallengeDetailResDTO(chlgObject: challenge, userProgress: progress)
-                return result(.success(detail))
-            
-            // Exception Handling: Invalid ChallengeType
-            case .failure(let error):
-                return result(.failure(error))
-            }
-            
-        }
-    }
-     */
-    
-    //===============================
-    // MARK: Select - MyChallenges
-    //===============================
-    /*TODO: Exception Handling
-    // * need return type?
-    func selecteMyChallenge(challengeID: Int, status: Bool,
-                            result: @escaping(Result<Bool, Error>) -> Void) {
+        // Update Statistics : Add MyChallenge
+        try await updateStatistics(challengeId)
         
-        // get ChallengeData
-        guard let reqChallenge = findChallengeByID(in: self.challengeArray, challengeId: challengeID) else {
-            return result(.failure(ChallengeError.InvalidChallengeId))
-        }
-        
-        // Update ChallengeData
-        let updatedChallenge = ChallengeStatusReqDTO(chlgObject: reqChallenge, myChlg: status)
-        chlgCD.updateChallengeStatus(updatedChallenge: updatedChallenge) { cdResult in
-            
-            switch cdResult {
-            case .success:
-                
-                // Adjust updated 'challengeArray'
-                self.initChallenge() { updateRes in
-                    
-                    switch updateRes {
-                    case .success:
-                        return result(.success(true))
-                    
-                    // Exception Handling : Failed to get ChallengeList
-                    case .failure(let error):
-                        return result(.failure(error))
-                    }
-                }
-            // Exception Handling : 'Invalid identifier' or 'Invalid challengeID
-            case .failure(let error):
-                return result(.failure(error))
-            }
-        }
-    }
-     */
-
-    
-    //===============================
-    // MARK: Get - Challenges
-    //===============================
-    func getChallenge(result: @escaping(Result<[ChallengeCardResDTO], Error>) -> Void) {
-        
-        // extract every challenges
-        let challenges = challengeArray.map { ChallengeCardResDTO(chlgObject: $0) }
-        
-        // return totalChallengeCard Array
-        result(challenges.isEmpty ? .failure(ChallengeError.InternalError) : .success(challenges))
+        // Refresh MyChallenge
+        try getMychallengesProcess()
     }
     
     //===============================
-    // MARK: Get - Challenge Detail
+    // MARK: Get MyChallenges
     //===============================
-    func getChallengeDetail(challengeId: Int,
-                            result: @escaping(Result<ChallengeDetailResDTO, Error>) -> Void) {
-        
-        // extract challenge, that 'chlg_selected' is true
-        guard let challenge = challengeArray.first(where: { $0.chlg_id == challengeId }) else {
-            return result(.failure(ChallengeError.InvalidChallengeId))
-        }
-        // get Privous Challenge Description
-        getPrevChallengeDesc(challengeType: challenge.chlg_type, currentStep: challenge.chlg_step) { descRes in
-            switch descRes {
-            
-            case .success(let desc):
-                let detail = ChallengeDetailResDTO(chlgObject: challenge, prevChallenge: desc)
-                return result(.success(detail))
-            
-                //Exception Handling: Failed to Search Previous Challenge
-            case .failure(let error):
-                return result(.failure(error))
-            }
-        }
+    func getMyChallenges() throws -> [MyChallengeDTO] {
+        return self.myChallenges ?? []
     }
     
-    // Extension
-    func getPrevChallengeDesc(challengeType: ChallengeType, currentStep: Int, result: @escaping(Result<String, Error>) -> Void) {
-        // Search Privous Challenge
-        if let prevChallenge = challengeArray.first(where: { $0.chlg_type == challengeType && $0.chlg_step == currentStep - 1 }) {
-            // return Previous Challenge Description
-            result(.success(prevChallenge.chlg_desc))
-        } else {
-            //Exception Handling: Failed to Search
-            result(.failure(ChallengeError.PreviousChallengeSearchFailed))
+    // ###### Core Function ######
+    private func getMychallengesProcess() throws {
+        
+        // Check Parameter
+        guard let stat = self.statistics else {
+            throw ChallengeError.UnexpectedStatError
         }
+        
+        // get MyChallenge from Total Challenge
+        var myChallenges: [MyChallengeDTO] = []
+        for idx in stat.stat_mychlg {
+            // Get Challenge
+            let myChallenge = try challengeCD.getChallenge(from: idx)
+            // Get UserProgress related Challenge
+            let userProgress = statCenter.userProgress(from: myChallenge.chlg_type, from: stat)
+            // Struct MyChallenge
+            myChallenges.append(MyChallengeDTO(chlgObject: myChallenge, userProgress: userProgress))
+        }
+        
+        // Set only first three items
+        self.myChallenges =  Array(myChallenges.prefix(3))
+    }
+    
+    //===============================
+    // MARK: Disable MyChallenge (Update)
+    //===============================
+    // Disable
+    func disableMyChallenge(from challengeId: Int) async throws {
+        // Search & Update Challenge
+        let updatedChallenge = ChallengeStatusDTO(target: challengeId, select: false, disableTime: Date())
+        try await challengeCD.updateChallenge(from: updatedChallenge)
+        
+        // Update Statistics : Remove MyChallenge
+        try await updateStatistics(challengeId, nil)
+        
+        // Refresh MyChallenge
+        try getMychallengesProcess()
+    }
+    
+    //===============================
+    // MARK: Reward MyChallenge (Update)
+    //===============================
+    // Reward
+    func rewardMyChallenge(from challengeId: Int) async throws -> ChallengeRewardDTO {
+        let updatedDate = Date()
+        
+        // Update Reward Challenge
+        try await rewardChallengeUpdate(challengeId, updatedDate)
+        
+        // Update Next Challenge
+        let nextChallenge = try await nextChallengeUpdate(challengeId)
+        
+        // Update Statistics
+        try await updateStatistics(challengeId, nextChallenge.chlg_type)
+        
+        // Update ChallengeLog
+        try await challengeLogCD.updateLog(to: self.userId, what: [challengeId : updatedDate], when: updatedDate)
+        
+        // Return Next Challenge
+        return ChallengeRewardDTO(from: nextChallenge)
     }
 }
 
 //===============================
-// MARK: - Support Function
+// MARK: - Support Func: Statistics
 //===============================
 extension ChallengeService {
-    func findChallengeByID(in challenges: [ChallengeObject], challengeId: Int) -> ChallengeObject? {
-        return challenges.first { $0.chlg_user_id == identifier }
+    
+    // Update Statistics (Add)
+    private func updateStatistics(_ id: Int) async throws {
+        // Check Parameter
+        guard var stat = self.statistics else {
+            throw ChallengeError.UnexpectedStatError
+        }
+        // Add Challenge to MyChallenge & Update Statistics
+        stat.stat_mychlg.append(id)
+        try await statisticsCD.updateStatistics(from: self.userId, to: stat)
+        
+        // Refresh Statistics
+        self.statistics = stat
+    }
+    
+    // Update Statistics (Remove)
+    private func updateStatistics(_ id: Int, _ type: ChallengeType?) async throws {
+        // Check Parameter
+        guard var stat = self.statistics else {
+            throw ChallengeError.UnexpectedStatError
+        }
+        // reward MyChallenge Active Only
+        if let type = type {
+            try updateChallengeStep(stat, type)
+        }
+        // Search Target Challenge at MyChallenges
+        guard let idx = stat.stat_mychlg.firstIndex(of: id) else {
+            throw ChallengeError.UnexpectedMyChallengeSearchError
+        }
+        // Remove Challenge from MyChallenges & Update
+        stat.stat_mychlg.remove(at: idx)
+        try await statisticsCD.updateStatistics(from: self.userId, to: stat)
+            
+        // Refresh Statistics
+        self.statistics = stat
+    }
+    
+    // Update Statistics: Challenge Step
+    private func updateChallengeStep(_ stat: StatisticsDTO, _ type: ChallengeType) throws {
+        // Search ChallengeStep Data
+        guard let idx = stat.stat_chlg_step.firstIndex(where: { $0.keys.contains(type.rawValue) } ),
+              let currentVal = stat.stat_chlg_step[idx][type.rawValue] else {
+            throw ChallengeError.UnexpectedStepUpdateError
+        }
+        // Update ChallengeStep Data
+        var updatedstat = stat
+        updatedstat.stat_chlg_step[idx][type.rawValue] = currentVal + 1
+        self.statistics = updatedstat
+    }
+}
+
+//===============================
+// MARK: - Support Func: Challenge
+//===============================
+extension ChallengeService {
+    // Update Challenge : Reward Target
+    private func rewardChallengeUpdate(_ id: Int, _ date: Date) async throws {
+        let updatedChallenge = ChallengeStatusDTO(target: id, when: date)
+        try await challengeCD.updateChallenge(from: updatedChallenge)
+    }
+
+    // Update Challenge : Next Target
+    private func nextChallengeUpdate(_ id: Int) async throws -> ChallengeObject {
+        // Search Next Challenge
+        guard let currentChallenge = self.challengeArray?.first(where: { $0.chlg_id == id }),
+              let nextChallenge = try getNextChallenge(challengeType: currentChallenge.chlg_type, currentStep: currentChallenge.chlg_step)
+        else {
+            throw ChallengeError.UnexpectedNextChallengeSearchError
+        }
+
+        // Struct DTO & Update
+        let updatedChallenge = ChallengeStatusDTO(target: nextChallenge.chlg_id, status: true)
+        try await challengeCD.updateChallenge(from: updatedChallenge)
+        return nextChallenge
+    }
+    
+    // Get Challenge : Next Target
+    private func getNextChallenge(challengeType: ChallengeType, currentStep: Int) throws -> ChallengeObject? {
+        // Check Parameter
+        guard let array = self.challengeArray else {
+            throw ChallengeError.UnexpectedChallengeArrayError
+        }
+        return array.first { $0.chlg_type == challengeType && $0.chlg_step == currentStep + 1 }
+    }
+}
+
+//===============================
+// MARK: Get Challenge
+//===============================
+extension ChallengeService {
+    func getChallenge(from id: Int) throws -> ChallengeDTO {
+        // Check Parameter
+        guard let array = self.challengeArray else {
+            throw ChallengeError.UnexpectedChallengeArrayError
+        }
+        // Search Challenge
+        guard let challenge = array.first(where: { $0.chlg_id == id }) else {
+            throw ChallengeError.UnexpectedGetError
+        }
+        // Search Prev Challenge
+        guard let prevChallenge = try getPrevChallenge(challengeType: challenge.chlg_type, currentStep: challenge.chlg_step) else {
+            return ChallengeDTO(from: challenge)
+        }
+        return ChallengeDTO(from: challenge, prev: prevChallenge)
+    }
+}
+
+//===============================
+// MARK: - Support Func
+//===============================
+extension ChallengeService {
+    func getPrevChallenge(challengeType: ChallengeType, currentStep: Int) throws -> ChallengeObject? {
+        // Check Parameter
+        guard let array = self.challengeArray else {
+            throw ChallengeError.UnexpectedChallengeArrayError
+        }
+        // Check Current Challenge
+        if currentStep <= 1 {
+            return nil
+            // Search Privous Challenge
+        } else if let prevChallenge = array.first(where: { $0.chlg_type == challengeType && $0.chlg_step == currentStep - 1 }) {
+            return prevChallenge
+        } else {
+            //Exception Handling: Failed to Search
+            throw ChallengeError.UnexpectedPrevChallengeSearchError
+        }
     }
 }
 
@@ -202,30 +268,30 @@ extension ChallengeService {
 // MARK: - Exception
 //===============================
 enum ChallengeError: LocalizedError {
-    case NoMyChallenge
-    case ChallengeIdGetFailed
-    case IdentifierGetFailed
-    case InvalidChallengeId
-    case InvalidType
-    case InternalError
-    case PreviousChallengeSearchFailed
+    case UnexpectedGetError
+    case UnexpectedStatError
+    case UnexpectedChallengeArrayError
+    case UnexpectedMyChallengeSearchError
+    case UnexpectedNextChallengeSearchError
+    case UnexpectedPrevChallengeSearchError
+    case UnexpectedStepUpdateError
     
     var errorDescription: String?{
         switch self {
-        case .NoMyChallenge:
-            return "No Selected Challenge for MyChallenge"
-        case .ChallengeIdGetFailed:
-            return "Failed to Get Challenge by ChallengeID"
-        case .IdentifierGetFailed:
-            return "Failed to Get Challenge by identifier"
-        case .InvalidChallengeId:
-            return "Invalid ChallengeID"
-        case .InvalidType:
-            return "Invalid ChallengeType"
-        case .InternalError:
-            return "Failed to get Challenge by InternalError"
-        case .PreviousChallengeSearchFailed:
-            return "Failed to Search Previous Challenge"
+        case .UnexpectedGetError:
+            return "Service: There was an unexpected error while Get Challenge in 'ChallengeService'"
+        case .UnexpectedStatError:
+            return "Service: There was an unexpected error while Check Statistics Data in 'ChallengeService'"
+        case .UnexpectedChallengeArrayError:
+            return "Service: There was an unexpected error while Check Challenge Array in 'ChallengeService'"
+        case .UnexpectedMyChallengeSearchError:
+            return "Service: There was an unexpected error while Search MyChallenge in 'ChallengeService'"
+        case .UnexpectedNextChallengeSearchError:
+            return "Service: There was an unexpected error while Search Next Challenge in 'ChallengeService'"
+        case .UnexpectedPrevChallengeSearchError:
+            return "Service: There was an unexpected error while Search Prev Challenge in 'ChallengeService'"
+        case .UnexpectedStepUpdateError:
+            return "Service: There was an unexpected error while Update ChallengeStep in 'ChallengeService'"
         }
     }
 }
