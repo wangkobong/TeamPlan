6ea6eefb18442f5dda68c27cffec1b806e5bd8a0
Hamin Song <cec7378@gmail.com>
2024-03-01 01:14:33 +0900

    [feat] UI 적용 완료

--- teamplan/Sources/Views/MyPage/MyPageView.swift
+++ teamplan/Sources/Views/MyPage/MyPageView.swift
@@ -8,86 +8,21 @@
 
 import SwiftUI
 
-struct MenuView: View {
-    let title: String
-    let showArrow: Bool
-    
-    var body: some View {
-        HStack {
-            Text(title)
-            Spacer()
-            if showArrow {
-                Image(systemName: "chevron.right")
-            }
-        }
-    }
-    
-    init(title: String, showArrow: Bool) {
-        self.title = title
-        self.showArrow = showArrow
-    }
-}
-
-struct AccomplishView: View {
-    
-    let accomplishTitle: String
-    let accomplishCount: Int
-    
-    var body: some View {
-        ZStack {
-            Spacer()
-            VStack(spacing: 6) {
-                Text(self.accomplishTitle)
-                    .font(.appleSDGothicNeo(.regular, size: 12))
-                Text("\(self.accomplishCount)")
-                    .font(.appleSDGothicNeo(.regular, size: 18))
-                    .foregroundStyle(.purple)
-            }
-            Spacer()
-        }
-        .frame(maxWidth: 132, maxHeight: 72)
-        .background(Color.white)
-        .overlay(
-            RoundedRectangle(cornerRadius: 0) // 사각형 테두리
-                .stroke(.clear, lineWidth: 1) // 테두리 색상 및 두께 설정
-                .shadow(radius: 5) // 테두리에 그림자 적용
-        )
-    }
-    
-    init(accomplishTitle: String, accomplishCount: Int) {
-        self.accomplishTitle = accomplishTitle
-        self.accomplishCount = accomplishCount
-    }
-}
-
-struct Accomplishment: Identifiable {
-    var id = UUID()
-    let accomplishTitle: String
-    let accomplishCount: Int
-}
-
 struct MyPageView: View {
     
-    var menuTitles: [String] = ["가이드북", "로그아웃", "회원탈퇴", "앱 버전"]
+    // MARK: - private properties
     
-    var sinceDays: Int = 1
-    var accomplishes: [Accomplishment] = [
-        .init(accomplishTitle: "완료 도전과제", accomplishCount: 10),
-        .init(accomplishTitle: "완료한 목표", accomplishCount: 12),
-        .init(accomplishTitle: "완료한 할 일", accomplishCount: 12)
-    ]
+    private var leftRightInset: CGFloat = 16
+    private var menuTitles: [String] = ["가이드북", "로그아웃", "회원탈퇴", "앱 버전"]
     
-    var savedBomb = 3
-    var disappearedBomb = 3
-    
-    var savedBombAttrString: AttributedString {
+    private var savedBombAttrString: AttributedString {
         let targetString = "\(self.savedBomb)개"
         var targetAttributed = AttributedString(targetString)
         targetAttributed.foregroundColor = .black
         return targetAttributed
     }
     
-    var disappearedBombAttrString: AttributedString {
+    private var disappearedBombAttrString: AttributedString {
         let targetString = "\(self.disappearedBomb)개"
         var targetAttributed = AttributedString(targetString)
         targetAttributed.foregroundColor = .black
@@ -95,18 +30,42 @@ struct MyPageView: View {
     }
     
     
-    var leftRightInset: CGFloat = 16
+    // MARK: - properties
+    
+    // FIXME: need data binding
+    
+    var sinceDays: Int = 1
+    var savedBomb = 3
+    var disappearedBomb = 3
+    
+    var accomplishes: [Accomplishment] = [
+        .init(accomplishTitle: "완료 도전과제", accomplishCount: 10),
+        .init(accomplishTitle: "완료한 목표", accomplishCount: 12),
+        .init(accomplishTitle: "완료한 할 일", accomplishCount: 12)
+    ]
+    
+    
+    // MARK: - body
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Spacer()
-                HStack {
+            VStack(alignment: .leading) {
+                Text("마이페이지")
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(.purple)
+                    .frame(height: 60)
+                
+                HStack(spacing: 20) {
                     Image("project_empty")
-                        .padding([.leading, .trailing])
+                    
                     VStack(alignment: .leading, spacing: 10) {
-                        Text("김하나 지킴이")
-                            .frame(alignment: .leading)
-                            .font(.appleSDGothicNeo(.bold, size: 20))
+                        HStack(alignment: .center, spacing: 4) {
+                            Text("김하나 지킴이")
+                                .frame(alignment: .leading)
+                                .font(.appleSDGothicNeo(.bold, size: 20))
+                            Image("Pencil")
+                        }
                         HStack {
                             Text(AttributedString("지켜낸 폭탄맨 ") + self.savedBombAttrString)
                                 .foregroundStyle(.purple)
@@ -114,57 +73,60 @@ struct MyPageView: View {
                                 .foregroundStyle(.purple)
                         }
                     }
-                    Spacer()
                 }
                 
-                Rectangle()
+                Divider()
+                    .background(.black.opacity(0.6))
                     .frame(height: 1)
-                    .foregroundStyle(.gray)
-                    .padding(.init(top: 28, leading: self.leftRightInset, bottom: 0, trailing: self.leftRightInset))
+                    .padding(.init(top: 20, leading: .zero, bottom: 0, trailing: .zero))
                 
                 VStack(alignment: .leading, spacing: 8) {
                     Text("투두팡과 함께한 추억")
                         .font(.title3)
                         .bold()
-                    Text("투두팡과 \(sinceDays)일 동안 함께 했어요") // bind actual data
                     HStack {
+                        Image("Time")
+                        Text("투두팡과 \(sinceDays)일 동안 함께 했어요")
                         Spacer()
                     }
                 }
-                .padding(.init(top: 18, leading: self.leftRightInset, bottom: .zero, trailing: self.leftRightInset))
+                .padding(
+                    .init(
+                        top: 18,
+                        leading: .zero,
+                        bottom: 10,
+                        trailing: .zero
+                    )
+                )
                 
-                HStack(spacing: 0) {
-                    ForEach(Array(accomplishes.enumerated()), id: \.element.id) { index, accomplish in
-                        AccomplishView(
-                            accomplishTitle: accomplish.accomplishTitle,
-                            accomplishCount: accomplish.accomplishCount
-                        )
-                        .padding(.leading, index == 0 ? self.leftRightInset : 0)
-                        .padding(.trailing, index == self.accomplishes.count - 1 ? self.leftRightInset : 0)
-                        
-                    }
-                }
-                
-                Rectangle()
+                AccomplishmentView(accomplishes: self.accomplishes)
+                    .frame(maxWidth: .infinity, minHeight: 78)
+
+                Divider()
+                    .background(.black.opacity(0.6))
                     .frame(height: 1)
-                    .foregroundStyle(.gray)
-                    .padding(.init(top: 30, leading: self.leftRightInset, bottom: 0, trailing: self.leftRightInset))
-                
-                ZStack {
-                    List {
-                        ForEach(menuTitles, id: \.self) { title in
-                            if title == "앱 버전" {
-                                MenuView(title: "앱 버전", showArrow: false)
-                            } else {
-                                MenuView(title: title, showArrow: true)
-                            }
-                        }
+                    .padding(
+                        .init(
+                            top: 30,
+                            leading: .zero,
+                            bottom: .zero,
+                            trailing: .zero
+                        )
+                    )
+            }
+            .padding(.init(top: 0, leading: self.leftRightInset, bottom: 0, trailing: self.leftRightInset))
+            
+            List {
+                ForEach(menuTitles, id: \.self) { title in
+                    if title == "앱 버전" {
+                        MenuItemView(title: "앱 버전", showArrow: false)
+                    } else {
+                        MenuItemView(title: title, showArrow: true)
                     }
-                    .listStyle(.plain)
                 }
             }
-            .background(.white)
-            .navigationTitle("마이페이지")
+            .scrollDisabled(true)
+            .listStyle(.plain)
         }
         
     }
